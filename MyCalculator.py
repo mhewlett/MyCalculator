089c6f364d7a6d5731ca6bee30635da56a9a0a32
mhewlett <mhewl@cox.net>
2023-01-07 19:46:55 -0500

    Update MyCalculator.py

--- MyCalculator.py
+++ MyCalculator.py
@@ -24,7 +24,7 @@ def exit(x):
 def check(m):
     exit(m)
     while m.lower() != 'y' and m.lower() != 'n':
-        print('Enter a valid response.')
+        print('Enter a valid response')
         m = input('(Y or N) ')
         exit(m)
     return m
@@ -32,7 +32,7 @@ def check(m):
 # Check if user is found in the directory
 print('Would you like to access the directory?')
 sfile = input('(Y or N) ')
-check(sfile)
+sfile = check(sfile)
 if sfile.lower() == 'n':
     print('Closing directory')
 elif sfile.lower() == 'y':
@@ -47,12 +47,12 @@ elif sfile.lower() == 'y':
     while username not in li:
         print('User not found')
         cont = input('Would you like to continue? (Y or N): ')
-        check(cont)
+        cont = check(cont)
         if cont.lower() == 'y':
             sfile = 'n'
             break
         else:
-            input('Closing program.')
+            input('Closing program')
             quit()
 
 # Navigate to directory
@@ -65,32 +65,33 @@ if sfile == 'y':
 response = 'y'
 while response.lower() == 'y':
 
-    # Ask user if they want to review previous entries
+    # Ask user if they want to review previous entries and open the text file
     if sfile.lower() == 'y':
-        read = input('Would you like to review the previous logged entries? (Y or N) ')
-        check(read)
+        read = input('\nWould you like to review the previous logged entries? (Y or N) ')
+        read = check(read)
         if read.lower() == 'y':
             with open('MyCalculations.txt', 'r') as f:
                 for line in f:
                     print(line)
 
     # Variable assignment and entry validation
-    print('\nEnter 2 digits'.upper())
+    print('\nNumber entry'.upper())
     num1 = input('Number 1: ')
     exit(num1)
     while not num1.isdigit:
         num1 = input('Enter a valid number: ')
-    num2 = input ('Number 2: ')
+        exit(num1)
+    num2 = input('Number 2: ')
     exit(num2)
     while not num2.isdigit():
         num2 = input('Enter a valid number: ')
         exit(num2)
-    print('\nNumber 1 is {}. Number 2 is {}.'.format(num1, num2))
+    print('\nNumber 1 is {}. Number 2 is {}'.format(num1, num2))
 
     # Choice of operation assignment
     choices = ['(1) Add', '(2) Subtract', '(3) Multiply', '(4) Divide', '(5) Exponential']
     print(choices)
-    x = input('Select an operation: ')
+    x = input('Enter the number associated with the operation you would like to run: ')
     exit(x)
 
     # Call function and cast to proper data type
@@ -101,15 +102,15 @@ while response.lower() == 'y':
     if sfile == 'y':
         print('\nWould you like to record these results?')
         record = input('(Y or N) ')
-        check(record)
+        record = check(record)
         if record.lower() == 'y':
             with open('MyCalculations.txt', 'a') as f:
                 f.write('\n')
                 f.write(calculated.rstrip('\n'))
 
     # Determine user's choice to continue or quit
-    print('\nRun program again?\n')
+    print('\nRun program again?')
     response = input('(Y or N) ')
-    check(response)
+    response = check(response)
     if response.lower() == 'n':
         input('Adios turd nuggets ;P')
